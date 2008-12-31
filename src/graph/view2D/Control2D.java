fa9873649d321d115778af812786f88f368adffa
doyenatccny <doyenatccny@4ce11633-b94a-0410-9451-6b31046aedaa>
2008-12-31 03:31:40 +0000

    Add initial graph directory.

---
+++ src/graph/view2D/Control2D.java
@@ -0,0 +1,35 @@
+package graph.view2D;
+
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * Control2D is the menu to control the different
+ * visual attributes of a graph.
+ * 
+ * @author Alfredo Portes
+ *
+ */
+
+public class Control2D extends JFrame {
+
+	public Control2D(){
+		
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Control2D c = new Control2D();
+		c.setTitle("The first Java 2D program");
+		c.setSize(350,80);
+		c.setVisible(true);
+
+	}
+
+}
---
+++ src/graph/view2D/Main2D.java
@@ -0,0 +1,13 @@
+package graph.view2D;
+
+public class Main2D {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
